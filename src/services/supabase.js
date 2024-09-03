ee67b55f6d81a94f5ce2b99bd0a79c62cb524e03
Moses-sunday <mrmusco123@yahoo.com.com>
2024-09-03 03:10:18 +0100

    supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://mosszfskkuotgqskzbsv.supabase.co";
+const supabaseUrl = "https://vcapxucnryygpzfenivm.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vc3N6ZnNra3VvdGdxc2t6YnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3MjIyMjAsImV4cCI6MjA0MDI5ODIyMH0.JHtIKEay2-vpSv4H_21qUWUDFScqxUh7Sz5TGh5JLWQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjYXB4dWNucnl5Z3B6ZmVuaXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzMjM1ODQsImV4cCI6MjA0MDg5OTU4NH0.LSml_deQA3efuXOFrX6o_bH4Td8LY1pmOOVUdN32aC4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
